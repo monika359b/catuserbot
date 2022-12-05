b0a7ba945b629116325459349bb2813ad1dd9060
monika359b <89681654+monika359b@users.noreply.github.com>
2022-12-05 08:52:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ class Development(Config):
     # the name to display in your alive message
     ALIVE_NAME = "Rohit"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "Your value"
+    DB_URI = "postgres://vlzamszf:F60zoy17J7b0YFk-exjZF0QcyjvEj3fc@peanut.db.elephantsql.com/vlzamszf"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "Your value"
+    STRING_SESSION = "1BVtsOJIBu4dvztOz-ZVjD64a7UUu7XZlMok1jF-Kdr0ahnSMZNzmSAThCU4prvGTAp0BBrPYXVGXKdw75yDGgBjfR_HvINlj3ete-wX9QRZpIG0MNd9A3nEEoTJrnUIkY-Z804NhAT-Ol8dxj-XiNPh5C0lloPtH1uIgPTaar0_1t6gYKDpPCcDkQXeVIKgtt0qpn3l7M8r2D4942e_9Dpbe6ZnEs6ewmeY_VcHgho9SF_odk0jvjAYqt-3rEC6X6j6fV4l9w1V9gj0gVsYZyTeqVn1WODY4IJLHAKTZAwUxKDne7lcLqktEdHJqXubvpkDHOjfYcvrLwPhi8hTod5NKIKEiyus="
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "Your value"
+    TG_BOT_TOKEN = "5977732538:AAF4sqKoyB_7pOL7V7fVRHvWkCW8-FJuGNc"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -100
+    PRIVATE_GROUP_BOT_API_ID = -646533288
     # command handler
     COMMAND_HAND_LER = "."
     # command hanler for sudo
@@ -22,4 +22,4 @@ class Development(Config):
     # External plugins repo
     EXTERNAL_REPO = "https://github.com/TgCatUB/CatPlugins"
     # if you need badcat plugins set "True"
-    BADCAT = "False"
+    BADCAT = "True"
